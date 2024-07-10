386580a691b10e1aa62491da04d8f7a682f913c8
anilcorekci <35172975+anilcorekci@users.noreply.github.com>
2024-07-10 11:16:52 +0300

    ** Broken accounting of active state for widget
    (Wiki Editor:2851584): Gtk-WARNING **: 11:11:30.032: Broken accounting of active state for widget 0x4629520(GtkImage)
    
    (Wiki Editor:2851584): Gtk-WARNING **: 11:11:30.032: Broken accounting of active state for widget 0x464e640(GtkCenterBox)
    
    (Wiki Editor:2851584): Gtk-WARNING **: 11:11:30.032: Broken accounting of active state for widget 0x46640c0(GtkGizmo)

--- pyler/file_operations.py
+++ pyler/file_operations.py
@@ -190,10 +190,15 @@ class FileOperation():
         close operation for both tab and toplevel window
         """
         def close():
+            for each in get_n_widget:
+                GLib.idle_add(each.unrealize)
+
             if self.notebook.get_n_pages() == 1:
                 GLib.idle_add( self.notebook.set_show_tabs, False)
                 if self.gl_b["overlay"]:
                     self.gl_b["overlay"].set_margin_top(0)
+            else:
+                GLib.idle_add(self.notebook.remove_page, i)
 
         i=-1
         while i < self.notebook.get_n_pages():
@@ -243,14 +248,13 @@ class FileOperation():
                         case 1:
                             pass
                         case 0:
-                            GLib.idle_add(self.notebook.remove_page, i)
                             GLib.idle_add(close)
 
                 dialog.choose(self.wikieditor, None, resp)
           #      self.add_custom_styling(dialog)
 
             else:
-                GLib.idle_add(self.notebook.remove_page, i)
+                GLib.idle_add(close)
 
             break
 
@@ -317,7 +321,7 @@ class FileOperation():
 #            wiki_text = self.wikieditor.current_editor.get_parent()
 #            wiki_text.for_coding(False)
 #            wiki_text.space_drawer(False)
-            hata("\n#bug in sourceview feauture\nFile tpye not supported\nError Message:\n"
+            hata("\n#bug in sourceview feauture\nNot supperted file_tpye\nError Message:\n"
                  "GtkSourceView-CRITICAL **: 13:48:43.162:\n"
                  "Highlighting a single line took too much time,\n"
                  "syntax highlighting will be disabled\n\n\n", self.wikieditor)
